5d725e69c6954206a4138ebb15986fd759df8222
fgfsdd <159648512+muathalzoubi@users.noreply.github.com>
2024-02-20 00:36:08 +0000

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ChatArea, { Opp } from "./ChatArea";
 import "./styles.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ export default function App() {
         <div style={{ color: "white", fontSize: 20 }}>
           Simple Chat Layout Demo
         </div>
+        <Analytics />
         <span style={{ color: "aqua" }}>Powered by React js</span>
       </div>
 
